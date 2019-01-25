c7bd3a43162dab0c7c120d5028807d708d0cfd97
Daniel Duberg <danielduberg@gmail.com>
2019-01-25 15:15:19 +0100

    Added image_maxing to clear free space

--- rpl_exploration/src/rpl_exploration.cpp
+++ rpl_exploration/src/rpl_exploration.cpp
@@ -15,17 +15,17 @@
 
 #include <geometry_msgs/Pose.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "exploration");
   ros::NodeHandle nh;
   ROS_INFO("Started exploration");
- 
+
   // Open logfile;
   std::string path = ros::package::getPath("rpl_exploration");
   std::ofstream logfile, pathfile;
-  logfile.open (path + "/data/logfile.csv");
-  pathfile.open (path + "/data/path.csv");
+  logfile.open(path + "/data/logfile.csv");
+  pathfile.open(path + "/data/path.csv");
 
   ros::Publisher pub(nh.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 1000));
   ros::ServiceClient coverage_srv = nh.serviceClient<aeplanner_evaluation::Coverage>("/get_coverage");
@@ -35,13 +35,13 @@ int main(int argc, char** argv)
   actionlib::SimpleActionClient<rpl_exploration::FlyToAction> ac("fly_to", true);
   // ac.waitForServer(); //will wait for infinite time
   // ROS_INFO("Fly to ction server started!");
-  
+
   // wait for aep server to start
   ROS_INFO("Waiting for aeplanner action server");
   actionlib::SimpleActionClient<aeplanner::aeplannerAction> aep_ac("make_plan", true);
   aep_ac.waitForServer(); //will wait for infinite time
   ROS_INFO("aeplanner action server started!");
-  
+
   // wait for fly_to server to start
   ROS_INFO("Waiting for rrt action server");
   actionlib::SimpleActionClient<rrtplanner::rrtAction> rrt_ac("rrt", true);
@@ -52,8 +52,8 @@ int main(int argc, char** argv)
   ros::Duration(5.0).sleep();
 
   double initial_positions[8][4] = {
-    { 0,  0, 5.0,  0},
-    { 1.0,  0, 5.0,  0},
+      {0, 0, 2.0, 0},
+      {1.0, 0, 2.0, 0},
   };
 
   // This is the initialization motion, necessary that the known free space allows the planning
@@ -61,12 +61,13 @@ int main(int argc, char** argv)
   ROS_INFO("Starting the planner: Performing initialization motion");
   geometry_msgs::PoseStamped last_pose;
 
-  for (int i = 0; i < 2; ++i) {
+  for (int i = 0; i < 2; ++i)
+  {
     rpl_exploration::FlyToGoal goal;
     goal.pose.pose.position.x = initial_positions[i][0];
     goal.pose.pose.position.y = initial_positions[i][1];
     goal.pose.pose.position.z = initial_positions[i][2];
-    goal.pose.pose.orientation = tf::createQuaternionMsgFromYaw(initial_positions[i][3]);	
+    goal.pose.pose.orientation = tf::createQuaternionMsgFromYaw(initial_positions[i][3]);
     last_pose.pose = goal.pose.pose;
 
     ROS_INFO_STREAM("Sending initial goal...");
@@ -80,7 +81,8 @@ int main(int argc, char** argv)
   int actions_taken = 1;
 
   ros::Time start = ros::Time::now();
-  while (ros::ok()) {
+  while (ros::ok())
+  {
     ROS_INFO_STREAM("Planning iteration " << iteration);
     aeplanner::aeplannerGoal aep_goal;
     aep_goal.header.stamp = ros::Time::now();
@@ -89,15 +91,17 @@ int main(int argc, char** argv)
     aep_goal.actions_taken = actions_taken;
     aep_ac.sendGoal(aep_goal);
 
-    while(aep_ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED){
+    while (aep_ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
       ros::Duration(0.05).sleep();
       pub.publish(last_pose);
     }
 
     geometry_msgs::Pose path = aep_ac.getResult()->pose;
-    
+
     ros::Duration fly_time;
-    if(aep_ac.getResult()->clear.data){
+    if (aep_ac.getResult()->clear.data)
+    {
       actions_taken = 0;
 
       ros::Time s = ros::Time::now();
@@ -109,37 +113,42 @@ int main(int argc, char** argv)
       rpl_exploration::FlyToGoal goal;
       goal.pose.pose = goal_pose;
       ac.sendGoal(goal);
-      while(ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED){
+      while (ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED)
+      {
         ros::Duration(0.05).sleep();
       }
 
       ros::Time e = ros::Time::now();
       fly_time = e - s;
     }
-    else{
+    else
+    {
       rrtplanner::rrtGoal rrt_goal;
       rrt_goal.header.stamp = ros::Time::now();
       rrt_goal.header.frame_id = "map";
       rrt_goal.start = last_pose.pose;
-      if(!aep_ac.getResult()->frontiers.poses.size())
+      if (!aep_ac.getResult()->frontiers.poses.size())
       {
         ROS_WARN("Exploration complete!");
         break;
       }
-      for(auto it  = aep_ac.getResult()->frontiers.poses.begin();
-               it != aep_ac.getResult()->frontiers.poses.end(); ++it){
+      for (auto it = aep_ac.getResult()->frontiers.poses.begin();
+           it != aep_ac.getResult()->frontiers.poses.end(); ++it)
+      {
 
         rrt_goal.goal_poses.push_back(*it);
       }
       rrt_ac.sendGoal(rrt_goal);
-      while(rrt_ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED){
+      while (rrt_ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED)
+      {
         ros::Duration(0.05).sleep();
         pub.publish(last_pose);
       }
       std::vector<geometry_msgs::Pose> path = rrt_ac.getResult()->path;
 
       ros::Time s = ros::Time::now();
-      for(int i = path.size()-1; i >= 0; --i){
+      for (int i = path.size() - 1; i >= 0; --i)
+      {
         geometry_msgs::Pose goal_pose = path[i];
         // Write path to file
         pathfile << goal_pose.position.x << ", " << goal_pose.position.y << ", " << goal_pose.position.z << ", f" << std::endl;
@@ -148,7 +157,8 @@ int main(int argc, char** argv)
         rpl_exploration::FlyToGoal goal;
         goal.pose.pose = goal_pose;
         ac.sendGoal(goal);
-        while(ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED){
+        while (ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED)
+        {
           ros::Duration(0.05).sleep();
         }
       }
@@ -160,22 +170,24 @@ int main(int argc, char** argv)
     ros::Duration elapsed = ros::Time::now() - start;
 
     aeplanner_evaluation::Coverage srv;
-    if(!coverage_srv.call(srv)) { ROS_ERROR("Failed to call coverage service"); }
+    if (!coverage_srv.call(srv))
+    {
+      ROS_ERROR("Failed to call coverage service");
+    }
 
-    ROS_INFO_STREAM("Iteration: "       << iteration << "  " <<
-                    "Time: "            << elapsed << "  " <<
-                    "Sampling: "        << aep_ac.getResult()->sampling_time.data << "  " <<
-                    "Planning: "        << aep_ac.getResult()->planning_time.data << "  " <<
-                    "Collision check: " << aep_ac.getResult()->collision_check_time.data  << "  " <<
-                    "Flying: "          << fly_time << " " <<
-                    "Tree size: "       << aep_ac.getResult()->tree_size  << " " <<
-                    "Coverage: "        << srv.response.coverage << " " <<
-                    "F:     "           << srv.response.free << " " <<
-                    "O: "               << srv.response.occupied << " " <<
-                    "U: "               << srv.response.unmapped 
-                    );
+    ROS_INFO_STREAM("Iteration: " << iteration << "  "
+                                  << "Time: " << elapsed << "  "
+                                  << "Sampling: " << aep_ac.getResult()->sampling_time.data << "  "
+                                  << "Planning: " << aep_ac.getResult()->planning_time.data << "  "
+                                  << "Collision check: " << aep_ac.getResult()->collision_check_time.data << "  "
+                                  << "Flying: " << fly_time << " "
+                                  << "Tree size: " << aep_ac.getResult()->tree_size << " "
+                                  << "Coverage: " << srv.response.coverage << " "
+                                  << "F:     " << srv.response.free << " "
+                                  << "O: " << srv.response.occupied << " "
+                                  << "U: " << srv.response.unmapped);
 
-    logfile << iteration << ", " 
+    logfile << iteration << ", "
             << elapsed << ", "
             << aep_ac.getResult()->sampling_time.data << ", "
             << aep_ac.getResult()->planning_time.data << ", "
@@ -183,10 +195,10 @@ int main(int argc, char** argv)
             << fly_time << ", "
             << srv.response.coverage << ", "
             << srv.response.free << ", "
-            << srv.response.occupied << ", "            
+            << srv.response.occupied << ", "
             << srv.response.unmapped << ", "
             << aep_ac.getResult()->tree_size << std::endl;
-            
+
     iteration++;
   }
 
